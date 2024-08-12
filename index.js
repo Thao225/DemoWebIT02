2529645da70c86b6b127962e0162a203341ba04c
Thao225 <buithienhuongthao2005@gmail.com>
2024-08-13 03:50:12 +0700

    up

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-// componentDidMount() {
-//     // JS
-//     var video = document.querySelector("video"),
-//       container = document.querySelector("#videoContainer");
-  
-//     var setVideoDimensions = function() {
-//       var w = video.videoWidth;
-//       var videoRatio = (w / h).toFixed(2);
+// JS
+var video = document.querySelector("video"),
+container = document.querySelector("#container");
 
-//       var containerStyles = window.getComputedStyle(container),
-//         minW = parseInt(containerStyles.getPropertyValue("width")),
-//         minH = parseInt(containerStyles.getPropertyValue("height"));
-//       var widthRatio = minW / w,
-//         heightRatio = minH / h;
-//       let newWidth = 0;
-//       let newHeight = 0;
-//       if (widthRatio > heightRatio) {
-//         newWidth = minW;
-//         newHeight = Math.ceil(newWidth / videoRatio);
-//       } else {
-//         newHeight = minH;
-//         newWidth = Math.ceil(newHeight * videoRatio);
-//       }
-  
-//       video.style.width = newWidth + "px";
-//       video.style.height = newHeight + "px";
-//     };
-//     video.addEventListener("loadedmetadata", setVideoDimensions, false);
-//     window.addEventListener("resize", setVideoDimensions, false);
-// }
+var setVideoDimensions = function() {
+var w = video.videoWidth;
+var videoRatio = (w / h).toFixed(2);
+
+var containerStyles = window.getComputedStyle(container),
+  minW = parseInt(containerStyles.getPropertyValue("width")),
+  minH = parseInt(containerStyles.getPropertyValue("height"));
+var widthRatio = minW / w,
+  heightRatio = minH / h;
+let newWidth = 0;
+let newHeight = 0;
+if (widthRatio > heightRatio) {
+  newWidth = minW;
+  newHeight = Math.ceil(newWidth / videoRatio);
+} else {
+  newHeight = minH;
+  newWidth = Math.ceil(newHeight * videoRatio);
+}
+
+video.style.width = newWidth + "px";
+video.style.height = newHeight + "px";
+};
+video.addEventListener("loadedmetadata", setVideoDimensions, false);
+window.addEventListener("resize", setVideoDimensions, false);
